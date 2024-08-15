49dbe12924ec125fb3ce5caf168262b4f2b8f375
yurimses <yurimelo89@gmail.com>
2024-08-15 14:18:00 -0300

    tratamento csv

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TrabalhoMachineLearning
 Trabalho para a Disciplina de Machine Learning do Prof. Jefferson
+
+pip install -r requirements.txt
---
+++ tratamentocsv.py
@@ -0,0 +1,123 @@
+import os
+import pandas as pd
+import numpy as np
+
+def tratarIdade(idade):
+
+    # considera apenas os 2 últimos dígitos se for um número de 4 dígitos
+    if pd.notna(idade):
+        try:
+            idade_str = str(int(idade))  
+            if len(idade_str) == 4:  
+                return int(idade_str[-2:])  
+        except ValueError:
+            pass
+
+    return idade  
+
+
+def filtroFeatures(input_csv, output_csv):
+
+    # features que queremos usar
+    features = [
+        'CS_SEXO', 'NU_IDADE_N', 'CS_RACA', 'CS_ESCOL_N', 'PUERPERA',
+        'CARDIOPATI', 'SIND_DOWN', 'HEPATICA', 'NEUROLOGIC', 'PNEUMOPATI', 
+        'IMUNODEPRE', 'RENAL', 'OBESIDADE', 'OUT_MORBI', 'EVOLUCAO'
+    ]
+    
+    # ler o arquivo csv
+    df = pd.read_csv(input_csv, engine='python', encoding='ISO-8859-1', delimiter=';')
+
+    # filtrar as instâncias onde SG_UF_NOT é igual a 15 (PARÁ)
+    df_filtered = df[df['SG_UF_NOT'] == 15]
+
+    # filtrar por não evoluiu a óbito
+    df_filtered = df_filtered[df_filtered['EVOLUCAO'] == 1.0]
+    
+    # filtrar as colunas desejadas no DataFrame já filtrado
+    df_filtered = df_filtered[features]
+
+    # substituir strings vazias por NaN
+    df_filtered.replace('', np.nan, inplace=True)
+    
+    # remover linhas com valores ausentes ou NaN
+    df_filtered = df_filtered.dropna()
+
+    # aplicar o tratamento na coluna 'NU_IDADE_N'
+    df_filtered['NU_IDADE_N'] = df_filtered['NU_IDADE_N'].apply(tratarIdade)
+    
+    # salvar o novo DataFrame filtrado em um novo arquivo CSV
+    df_filtered.to_csv(output_csv, index=False)
+    
+    return df_filtered
+
+def processarArquivosNaPasta(input_folder, output_folder):
+
+    # verifica se a pasta de saída existe, se não tiver, cria
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    # verifica todos os arquivos .,csv
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".csv"):
+            input_csv = os.path.join(input_folder, filename)
+            output_csv = os.path.join(output_folder, filename.replace('.csv', '_filtrado.csv'))
+            try:
+                # aplica a função de filtragem em cada arquivo
+                filtroFeatures(input_csv, output_csv)
+                print(f"Processado: {filename}")
+            except Exception as e:
+                print(f"Erro ao processar {filename}: {e}")
+
+def agregarArquivos(output_folder, output_subfolder):
+
+    # cria a subpasta se não existir
+    full_output_folder = os.path.join(output_folder, output_subfolder)
+    if not os.path.exists(full_output_folder):
+        os.makedirs(full_output_folder)
+
+    # armazena os DataFrames
+    dfs = []
+
+    # verifica todos os arquivos CSV na pasta de saída
+    for filename in os.listdir(output_folder):
+        if filename.endswith("_filtrado.csv"):
+            file_path = os.path.join(output_folder, filename)
+            try:
+                # lê cada arquivo CSV e adiciona o DataFrame à lista
+                df = pd.read_csv(file_path, engine='python', encoding='ISO-8859-1', delimiter=',')
+
+                # remover colunas vazias ou com todos os valores NA
+                df = df.dropna(axis=1, how='all')
+
+                # remover linhas com valores ausentes
+                df = df.dropna()
+
+                dfs.append(df)
+                print(f"Adicionado: {filename}")
+            except Exception as e:
+                print(f"Erro ao agregar {filename}: {e}")
+
+    # junta todos os DataFrames em um único DataFrame
+    df_aggregated = pd.concat(dfs, ignore_index=True)
+
+    # define o caminho para o arquivo agregado
+    output_file = os.path.join(full_output_folder, 'dados_agregados.csv')
+    
+    # salva o DataFrame agregado em um novo arquivo CSV
+    df_aggregated.to_csv(output_file, index=False)
+
+    return df_aggregated
+
+# pata de entrada com csv brutos
+input_folder = '/home/yuri/Downloads/dt_influ'
+
+# pasta de saída com cvs tratados
+output_folder = '/home/yuri/Downloads/dt_influ_test'  
+processarArquivosNaPasta(input_folder, output_folder)
+
+# agrupar todos os csv em um só
+output_subfolder = 'CSV Agegrado'
+agregarArquivos(output_folder, output_subfolder)
+
+
